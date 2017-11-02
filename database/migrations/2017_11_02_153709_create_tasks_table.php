ea3208e49eb40b26caf6503f0dd993c69856f39d
David Costa <davidcostadev@gmail.com>
2017-11-02 14:40:00 -0300

    creating migration of tasks

---
+++ database/migrations/2017_11_02_153709_create_tasks_table.php
@@ -0,0 +1,44 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateTasksTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('tasks', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('name');
+            $table->integer('created_by')->unsigned();
+            $table->boolean('completed')->default(false);
+            $table->integer('completed_by')->unsigned()->nullable();
+            $table->timestamps();
+
+            $table->foreign('created_by')
+                ->references('id')->on('users')
+                ->onDelete('cascade');
+
+            $table->foreign('completed_by')
+                ->references('id')->on('users')
+                ->onDelete('cascade');
+
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('tasks');
+    }
+}
